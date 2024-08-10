93d2507f86743d214ff3ad04d583d6336eb3995b
SaiSoeSan <33777412+SaiSoeSan@users.noreply.github.com>
2024-08-10 08:00:31 -0500

    env file

--- server/server.js
+++ server/server.js
@@ -36,10 +36,13 @@
 //     console.log(`Server is running on localhost:${PORT}`)
 // })
 
+require('dotenv').config()
 const { MongoClient } = require("mongodb");
 
 // Replace the uri string with your connection string.
-const uri = "mongodb+srv://saisoesan:oeUF6oTbrCqLMx2r@movieprojectcluster.bwogg.mongodb.net/?retryWrites=true&w=majority&appName=MovieProjectCluster";
+// const uri = "mongodb+srv://saisoesan:oeUF6oTbrCqLMx2r@movieprojectcluster.bwogg.mongodb.net/?retryWrites=true&w=majority&appName=MovieProjectCluster";
+
+const uri = process.env.CONNECTION_STRING
 
 const client = new MongoClient(uri);
 
