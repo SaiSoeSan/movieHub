10a5d0d58f2eb23c2167113a0efe0165202aa231
SaiSoeSan <33777412+SaiSoeSan@users.noreply.github.com>
2024-08-09 22:30:10 -0500

    connect with mongodb

--- server/server.js
+++ server/server.js
@@ -1,37 +1,61 @@
-const express = require('express')
-const app = express();
-let path = require('path')
-const cors = require('cors');
+// const express = require('express')
+// const app = express();
+// let path = require('path')
+// const cors = require('cors');
 
-// Server port
-const PORT = process.env.PORT || 8000;
+// // Server port
+// const PORT = process.env.PORT || 8000;
 
-const options = {
-    origin : 'http://localhost:3000',
-    method : 'POST,GET,DELETE,PUT',
-    allowedHeaders : 'Content-Type'
-};
+// const options = {
+//     origin : 'http://localhost:3000',
+//     method : 'POST,GET,DELETE,PUT',
+//     allowedHeaders : 'Content-Type'
+// };
 
-//use cors
-app.use(cors(options));
-
-
-//get parameter from request
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+// //use cors
+// app.use(cors(options));
 
 
-// use static for react
-// app.use(express.static(path.join(__dirname,"../client/build")))
+// //get parameter from request
+// app.use(express.json())
+// app.use(express.urlencoded({extended:true}))
 
-// app.use('*',(req,res) => {
-//     res.sendFile(path.join(__dirname,"../client/build","index.html"))
+
+// // use static for react
+// // app.use(express.static(path.join(__dirname,"../client/build")))
+
+// // app.use('*',(req,res) => {
+// //     res.sendFile(path.join(__dirname,"../client/build","index.html"))
+// // })
+
+// app.get('/',(req,res) => {
+//     res.send("hello")
 // })
 
-app.get('/',(req,res) => {
-    res.send("hello")
-})
+// app.listen(PORT,(req,res) => {
+//     console.log(`Server is running on localhost:${PORT}`)
+// })
 
-app.listen(PORT,(req,res) => {
-    console.log(`Server is running on localhost:${PORT}`)
-})
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = "mongodb+srv://saisoesan:oeUF6oTbrCqLMx2r@movieprojectcluster.bwogg.mongodb.net/?retryWrites=true&w=majority&appName=MovieProjectCluster";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
