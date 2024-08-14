61fd486b249c701300c5a77eb3367cf80c0d75fb
SaiSoeSan <33777412+SaiSoeSan@users.noreply.github.com>
2024-08-13 19:43:54 -0500

    login and sign up design

--- client/src/Movie/Detail/Detail.js
+++ client/src/Movie/Detail/Detail.js
@@ -118,31 +118,6 @@ export default function Detail() {
 
     return (
         <div style={{ backgroundColor: 'black' }}>
-            <nav
-                className="navbar navbar-expand-lg sticky-top"
-                style={{ backgroundColor: "#000" }}
-            >
-                <div className="container-fluid">
-                    <a className="navbar-brand p-0" href="/">
-                        <img src="/images/logo.png" alt="" width="110px" height="60px" />
-                    </a>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-
-                    </div>
-                </div>
-            </nav>
             <div style={{
                 display: 'grid',
                 gridTemplateColumns: '2fr 3fr',
